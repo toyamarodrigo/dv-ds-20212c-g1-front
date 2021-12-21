202188185a54e88fbb72ac34ff2a1f8cb440539f
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-12-21 18:47:57 -0300

    minW 100%

--- src/layout/BasicLayout/BasicLayout.tsx
+++ src/layout/BasicLayout/BasicLayout.tsx
@@ -12,9 +12,9 @@ export const BasicLayout = ({ children }) => {
         h="100%"
         justifyContent="center"
         minH="90vh"
+        minW="100%"
         position="relative"
         spacing={10}
-        w="100vw"
       >
         <Stack alignItems="center" justifyContent="center" spacing={6} textAlign="center">
           {children}
